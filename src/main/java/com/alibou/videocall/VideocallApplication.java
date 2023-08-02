4b7400f694d006b668a86c33504bc54665e6e990
ali-bouali <bouali.pro@gmail.com>
2023-08-02 19:22:05 +0200

    init project

---
+++ src/main/java/com/alibou/videocall/VideocallApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.videocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideocallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideocallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibou/videocall/VideocallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.videocall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideocallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
